d8b4ae484c3f5a80a11a50a071082e41e9d42496
André R. de Melo <andrermelo1@hotmail.com>
2022-03-02 22:03:40 -0300

    aula 0821 pronta

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,7 +1,16 @@
 import React from "react";
+import FeedModal from "./FeedModal";
+import FeedPhotos from "./FeedPhotos";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [modalPhoto, setModalPhoto] = React.useState(null);
+
+  return (
+    <div>
+      {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
+    </div>
+  );
 };
 
 export default Feed;
